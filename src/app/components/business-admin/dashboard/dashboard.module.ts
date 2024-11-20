810cc28d28b7447b110ece45ab40ad5b10ab5420
unknown <kaleemikennels@gmail.com>
2024-11-20 14:28:27 +0500

    new changes

--- src/app/components/business-admin/dashboard/dashboard.module.ts
+++ src/app/components/business-admin/dashboard/dashboard.module.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardMainComponent } from './dashboard-main/dashboard-main.component';
-import { ActivitiesComponent } from './activities/activities.component';
 import { StatscardsComponent } from './statscards/statscards.component';
-import { SupplierDetailTableComponent } from './supplier-detail-table/supplier-detail-table.component';
 import { OrderStatusChartComponent } from './order-status-chart/order-status-chart.component';
 import { VendorPerformanceTableComponent } from './vendor-performance-table/vendor-performance-table.component';
 import { MaterialModule } from 'src/app/material';
@@ -20,9 +18,7 @@ import { EmployeeExpendituresComponent } from './employee-expenditures/employee-
 @NgModule({
   declarations: [
     DashboardMainComponent,
-    ActivitiesComponent,
     StatscardsComponent,
-    SupplierDetailTableComponent,
     OrderStatusChartComponent,
     VendorPerformanceTableComponent,
     RequestsChartComponent,
